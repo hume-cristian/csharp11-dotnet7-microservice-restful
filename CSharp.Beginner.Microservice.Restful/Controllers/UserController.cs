b05b3c9fa98c0fd8880a9ec47d500eb42a0b5e3e
Hume Cristian <hume-cristian@outlook.com>
2024-11-18 23:50:33 -0300

    feat: Create project for ASP.NET Core 7.0.0 and C# 11.0
    Signed-off-by: Hume Cristian <hume-cristian@outlook.com>

---
+++ CSharp.Beginner.Microservice.Restful/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+using CSharp.Beginner.Microservice.Restful.Models;
+using CSharp.Beginner.Microservice.Restful.Services;
+
+namespace CSharp.Beginner.Microservice.Restful.Controllers;
+
+/**
+ *  UserController.cs
+ * 
+ *  Author   Hume Cristian
+ *  Version  1.0.0
+ *  Created  2024-11-18
+ *  Modified 2024-11-18
+ */
+[ApiController]
+[Route("csharp/api/v1/users")]
+public class UserController : ControllerBase
+{
+    #region [PRIVATE-FIELDS]
+    private readonly UserService _userService;
+    #endregion [PRIVATE-FIELDS]
+
+    #region [PUBLIC-CONSTRUCTORS]
+    public UserController()
+    {
+        _userService = new UserService();
+    }
+    #endregion [PUBLIC-CONSTRUCTORS]
+
+    #region [PUBLIC-METHODS]
+    [HttpPost]
+    public IActionResult Create(UserModel userModel)
+    {
+        Int32 id = _userService.Create(userModel);
+
+        return Created("/api/csharp/v1/users/" + id, new { Id = id });
+    }
+
+    [HttpGet]
+    public IActionResult FindAll()
+    {
+        List<UserModel> userModelList = _userService.FindAll();
+
+        return (userModelList != null ? (userModelList.Count > 0 ? Ok(userModelList) : NotFound()) : NotFound());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult FindById(Int128 id)
+    {
+        UserModel? userModel = _userService.FindById(id);
+
+        return (userModel != null ? Ok(userModel) : NotFound());
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(Int32 id, UserModel userModel)
+    {
+        _userService.Update(id, userModel);
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Int32 id)
+    {
+        _userService.Delete(id);
+
+        return Ok();
+    }
+    #endregion [PUBLIC-METHODS]
+
+}
+
+
---
+++ CSharp.Beginner.Microservice.Restful/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+ *  Program.cs
+ * 
+ *  Author   Hume Cristian
+ *  Version  1.0.0
+ *  Created  2024-11-18
+ *  Modified 2024-11-18
+ */
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
